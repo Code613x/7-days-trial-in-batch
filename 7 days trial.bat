4fbca1383931a825eeef2db3fa4286f6e4e17eba
Code613 <75413090+Code613x@users.noreply.github.com>
2023-04-28 18:53:29 +0200

    Update 7 days trial.bat

--- 7 days trial.bat
+++ 7 days trial.bat
@@ -1,9 +1,9 @@
 @echo off
 REM PROGRAM MADE BY CODE613x ON GITUB AND CODE613 ON YOUTUBE.
 
-REM Replace folder name with your folder name
+REM Please replace 'folder name' with the name of your actual folder.
 set datedir=%appdata%\Foldername
-REM Replace the number 7 with the deafult value of the trial.
+REM Please substitute the number '7' with the default value of the trial period.
 set trial_time_deafult=7
 if not exist "%datedir%" (
   mkdir "%datedir%"
@@ -30,12 +30,12 @@ set current_date=%current_date: =%
 set /a x=(%current_date%-%start_date%)
 if %x% lss %trial_time% (
 
-REM YOUR CODE IF THE TRIAL PEROID HAS NOT EXPIRED YET.
+REM Add your code for what should happen if the trial period has not yet expired.
 
 goto Yourprogram
 )
 
-REM YOUR CODE IF THE TRIAL HAS EXPIRED.
+REM Add your code for what should happen if the trial period has expired.
 
 Pause 
 exit
